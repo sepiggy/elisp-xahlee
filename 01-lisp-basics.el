5ca06d3feca7ae4a80f2b1c48e9cd88f9b91c795
sepiggy <jms1209@qq.com>
2024-05-29 17:12:20 +0800

    First commit

---
+++ 01-lisp-basics.el
@@ -0,0 +1,59 @@
+(+ 3 4)
+;; => 7
+
+(+ 1 2)
+;; => 3
+
+(message "Name is: %s" "Joe")
+;; => Name is: Joe
+
+(insert "something")somethingsomething
+;; => nil
+
+(warn "Name is: %s" "Joe")
+;; => t
+
+(length "abc")
+;; => 3
+
+(substring "abc123" 0 3)
+;; => abc
+
+(concat "some" "thing")
+;; => something
+
+(split-string "xy_007_cat" "_")
+;; => (xy 007 cat)
+
+(string-trim " abc     ")
+;; => abc
+
+(upcase "abc")
+;; => ABC
+
+(format "Name: %s, age: %d" "jane" 25)
+;; => Name: jane, age: 25
+
+(+ 4 5 1)
+;; => 10
+
+(- 9 2)
+;; => 7
+
+(- 9 2 3)
+;; => 4
+
+(* 2 3)
+;; => 6
+
+(* 2 3 2)
+;; => 12
+
+(/ 7 2)
+;; => 3
+
+(/ 7 2.)
+;; => 3
+
+(/ 7 2.0)
+;; => 3.5
