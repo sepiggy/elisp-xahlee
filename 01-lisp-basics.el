3f394a927aa6f53a89813d8dfe1a8285fca95df4
sepiggy <jms1209@qq.com>
2024-05-30 10:29:35 +0800

    Up to `Equality Test` ok.

--- 01-lisp-basics.el
+++ 01-lisp-basics.el
@@ -57,3 +57,141 @@
 
 (/ 7 2.0)
 ;; => 3.5
+
+(integerp 2)
+;; => t
+
+(integerp 2.)
+;; => t
+
+(integerp 2.0)
+;; => nil
+
+(floatp 2)
+;; => nil
+
+(floatp 2.)
+;; => nil
+
+(floatp 2.0)
+;; => t
+
+(float 3)
+;; => 3.0
+
+(truncate 3.3)
+;; => 3
+
+(truncate 3.8)
+;; => 3
+
+(floor 3.3)
+;; => 3
+
+(floor 3.8)
+;; => 3
+
+(ceiling 3.3)
+;; => 4
+
+(round 3.4)
+;; => 3
+
+(string-to-number "3")
+;; => 3
+
+(number-to-string 3)
+;; => 3
+
+(stringp (number-to-string 3))
+;; => t
+
+(format "%d" 3)
+;; => 3
+
+(if nil "true" "false")
+;; => false
+
+(if 'nil "true" "false")
+;; => false
+
+(if () "true" "false")
+;; => false
+
+(if '() "true" "false")
+;; => false
+
+(if (list) "true" "false")
+;; => false
+
+(if () "true" "false")
+;; => false
+
+(if t "true" "false")
+;; => true
+
+(if y "true" "false")
+;; => Error: (void-variable y)
+
+(if 0 "true" "false")
+;; => true
+
+(if "" "true" "false")
+;; => true
+
+(if (vector ) "true" "false")
+;; => true
+
+(and t nil)
+;; => nil
+
+(and t nil t t t t t)
+;; => nil
+
+(or t nil)
+;; => t
+
+(not t)
+;; => nil
+
+(not nil)
+;; => t
+
+(not 2)
+;; => nil
+
+(= 3 3)
+;; => t
+
+(= 3.8 3.8)
+;; => t
+
+(= 3 3.0)
+;; => t
+
+(= 3 3.1)
+;; => nil
+
+(= 3 3.00000000000)
+;; => t
+
+(= 3 3.00000000000000000000001)
+;; => t
+
+(= 3 3.000000000000001)
+;; => nil
+
+(/= 3 4)
+;; => t
+
+(string-equal "abc" "abc")
+;; => t
+
+(string-equal "abc" "Abc")
+;; => nil
+
+(string-equal 'abc 'abc)
+;; => t
+
+(string-equal "abc" 'abc)
+;; => t
